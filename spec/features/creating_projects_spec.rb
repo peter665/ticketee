f37dfc5c74f67855135d2aaf4f9b543c5828b046
Peter Mester <peto.mester@gmail.com>
2018-05-14 19:45:13 +0200

    Add ability to view a list of all projects

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -24,6 +24,6 @@ RSpec.feature "Users can create new projects" do
     click_button "Create project"
 
     expect(page).to have_content "Project has not been created"
-    expect(page).to have_content "Name can`t be blank!"
+    expect(page).to have_content "Name can't be blank"
   end
 end
