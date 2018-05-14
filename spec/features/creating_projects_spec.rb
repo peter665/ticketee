6699a8fad1e8c7a3b50405eb115b8a37f9f555c4
Peter Mester <peto.mester@gmail.com>
2018-05-14 20:37:08 +0200

    Projects can now be updated

--- spec/features/creating_projects_spec.rb
+++ spec/features/creating_projects_spec.rb
@@ -9,7 +9,7 @@ RSpec.feature "Users can create new projects" do
   scenario "with valid attributes" do
     fill_in "Name", with: "Sublime Text 3"
     fill_in "Description", with: "A text editor for everyone"
-    click_button "Create project"
+    click_button "Create Project"
 
     expect(page).to have_content "Project has been created"
 
@@ -21,7 +21,7 @@ RSpec.feature "Users can create new projects" do
   end
 
   scenario "when providing invalid attributes" do
-    click_button "Create project"
+    click_button "Create Project"
 
     expect(page).to have_content "Project has not been created"
     expect(page).to have_content "Name can't be blank"
---
+++ spec/features/editing_projects_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.feature('Users can edit existing projects') do
+  before do
+    @project = FactoryBot.create(:project, name:"Sublime Text 3")
+    visit "/"
+    click_link "Sublime Text 3"
+    click_link "Edit Project"
+  end
+
+  scenario('with valid attributes') do
+    fill_in 'Name', with: 'Atom'
+    click_button 'Update Project'
+    expect(page).to have_content 'Project has been updated.'
+    expect(page).to have_content 'Atom'
+  end
+
+  scenario('when providing invalid attriburtes') do
+    fill_in 'Name', with:''
+    click_button 'Update Project'
+    expect(page).to have_content 'Project has not been updated.'
+    expect(page.current_url).to eq project_url(@project)
+  end
+end
