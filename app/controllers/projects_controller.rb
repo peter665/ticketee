6291d324c311dbbb291abbd7e7e0e78f33eac39d
Peter Mester <peto.mester@gmail.com>
2018-05-15 19:02:21 +0200

    Add redirect for nonexistent project requests

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  before_action :set_project, only: [:show, :edit, :destroy, :update]
   def index
     @projects = Project.all
   end
@@ -20,15 +21,12 @@ class ProjectsController < ApplicationController
   end
 
   def show
-    @project = Project.find(params[:id])
   end
 
   def edit
-    @project = Project.find(params[:id])
   end
 
   def update
-    @project = Project.find(params[:id])
     if @project.update(project_params)
       flash[:notice] = "Project has been updated."
       redirect_to @project
@@ -39,7 +37,6 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project = Project.find(params[:id])
     if @project.delete
       flash[:notice] = 'Project has been deleted.'
       redirect_to projects_path
@@ -51,6 +48,14 @@ class ProjectsController < ApplicationController
   private
   def project_params
     params.require(:project).permit(:name, :description)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:alert] = 'The project you were looking for
+    could not be found.'
+    redirect_to projects_path
   end
 
 end
--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ProjectsController, type: :controller do
+  it 'handles a missing project correctly' do
+    get :show, params: {id: "not-here"}
 
+    expect(response).to redirect_to projects_path
+
+    expect(flash[:alert]).to eq 'The project you were looking for
+    could not be found.'
+  end
 end
